ddb628e6c7a001106cb482377984d8a8afb1aea3
Yvonne Haagsma <yceelie@gmail.com>
2024-03-24 19:53:44 +0100

    Added testcontainers

--- src/test/java/nl/lisaveldhuisen/les/LesAppApplicationTests.java
+++ src/test/java/nl/lisaveldhuisen/les/LesAppApplicationTests.java
@@ -2,6 +2,7 @@ package nl.lisaveldhuisen.les;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 
 @SpringBootTest
 class LesAppApplicationTests {
